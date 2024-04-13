faa463797f18fac0dd12ad295aeaf131218e12ff
Tanmay Jain <tanmayj28@gmail.com>
2024-04-14 02:25:06 +0530

    outputs js variables and dynamic styles

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,6 +1,17 @@
 <script>
-  import Fun from "./Fun.svelte";
+  let usefulVars = 'This should be available in HTML';
+  let name = 'Tanmay';
+  let favColor = 'blue';
+
+  setTimeout(function() {
+    favColor = 'yellow'
+  }, 2000)
 </script>
 
 <h1>Hello World!</h1>
-<Fun />
+<h2>
+  {usefulVars}
+
+  <h3>Name:</h3>
+  <p style="color: {favColor};">{name}</p>
+</h2>
