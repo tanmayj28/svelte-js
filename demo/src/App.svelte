1537d0e334ad4a9e3199ef48a1c319e854594f78
Tanmay Jain <tanmayj28@gmail.com>
2024-04-14 12:50:42 +0530

    Binding values to Input Fields and vars :: added range input and alters fontsize

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,25 +1,18 @@
 <script>
-  import Fun from "./Fun.svelte";
+  let name = 'Jain';
+  let disabled = false;
+  let fontSize = 30;
+
+  setTimeout(() => {
+    name = 'Jainism'
+  }, 5000);
 </script>
 
-<h1>Hello World!!</h1>
-<h2>This is H2 from App Component</h2>
+<h2>Hello World!</h2>
+<input type="range" bind:value={fontSize}>
+<h2>FontSize is: {fontSize}</h2>
+<h2 style="font-size: {fontSize}px;">Name is: {name}</h2>
+<!-- this won't update the actual value of variable when you update it from UI, so you need to bind it. -->
+<input type="text" bind:value={name}>
 
-<!-- Even though FUN component has H2 tag it won't be styled because styles are scoped inside the HTML component they are in. -->
-<Fun />
-
-<style>
-  h1 {
-    color: blue
-  }
-
-  h2 {
-    color: green
-  }
-
-  /* This is not a good way to do CSS, what you can do is create a global.css and import in main.js and write your styles in there. */
-
-  :global(h2) {
-    color: yellow
-  }
-</style>
+<button disabled={disabled}> Click Me! </button>
