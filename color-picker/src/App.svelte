16b2faedd67de63cf81cb925a8334c562ff77ca6
Tanmay Jain <tanmayj28@gmail.com>
2024-04-14 14:10:13 +0530

    adds color picker :: multiple Componenets, CSS Imports, Passing props

---
+++ color-picker/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import ColorSlider from "./ColorSlider.svelte";
+  import ColorDisplay from "./ColorDisplay.svelte";
+
+  let reddish = 10;
+  let greeinsh = 0;
+  let blueish = 0;
+</script>
+
+<div class="row">
+  <div class="column">
+    <h1>Color Picker</h1>
+  </div>
+</div>
+
+<div class="row">
+  <div class="column">
+    <form>
+      <ColorSlider colorName='Red' bind:colorValue={reddish}/>
+      <ColorSlider colorName='Green' bind:colorValue={greeinsh}/>
+      <ColorSlider colorName='Blue' bind:colorValue={blueish}/>
+    </form>
+  </div>
+</div>
+
+<ColorDisplay {reddish} {greeinsh} {blueish} />
---
+++ color-picker/src/ColorDisplay.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let reddish;
+  export let greeinsh;
+  export let blueish;
+</script>
+
+<div class="row">
+  <div class="column">
+    <div id="color" style="background-color: rgb({reddish}, {greeinsh}, {blueish});"></div>
+  </div>
+</div>
+
+<style>
+  #color {
+    height: 300px;
+    border: dashed 5px;
+  }
+</style>
