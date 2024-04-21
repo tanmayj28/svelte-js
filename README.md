8974e16bcddf3146dc55ac07a870990a806c7449
Tanmay Jain <tanmayj28@gmail.com>
2024-04-21 15:58:27 +0530

    reactive component :: pass by reference :: updates data as dependent data updates

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #### Svelte
 
 - It doesn't bundle and ship unused code from various libraries used.
+- Adding `export` ahead of variables make them exportable props.
--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,19 +1,34 @@
+<!-- Reactive Statement: So, if anything on the right changes, it would reload the variable on left or rerun the code as seen in below console.log logic. -->
+
 <script>
-  // You can pass on the custom event `on:form_submit` to child component's imported FormContainer component directly from here. You don't have to call any function from FormContainer.
+  let name = 'Tanmay';
+  let initName = name.toUpperCase();
+  $: capitalizedName = name.toUpperCase();
 
-  // What happens is as we are not responding to the custom event it passes it to the parent to respond to instanceof. So, it happens in Form component and since we are not reacting to the event it passes it to FormContainer, which also doesn't respond with a function, so that is further passed to it's parent App.svelte. You can even do the same for other events also like Clikc etc.
+  let red = 10;
+  let green = 0;
+  let blue = 0;
+  $: colorValue = color(red, green, blue);
 
-  // import Form from "./Form.svelte";
-  import FormContainer from "./FormContainer.svelte";
-  import ChildEvent from "./ChildEvent.svelte";
+  function color(r,g,b) {
+    return `rgb(${r},${g},${b})`;
+  }
 
-  function onFormSubmit(e) {
-    console.log(e);
-    const { name, age } = e.detail;
-    console.log(name, age, 'Event Details recieved!')
+  setTimeout(() => {
+    green += 10;
+  },1500);
+
+  $: console.log(green);
+  $: {
+    console.log(green, 'Green Value Updates');
+    console.log(red, 'Red Value Same');
   }
 </script>
 
-<!-- <Form on:form_submit={onFormSubmit}/> -->
-<FormContainer on:click={() => {alert('Button Clicked!');}} on:form_submit={onFormSubmit} />
-<ChildEvent on:custom_event={(e) => alert(e.detail)} />
+<h2>Name: {name}</h2>
+<h2>Name: {initName}</h2>
+<h2>Name: {capitalizedName}</h2>
+
+<input type="text" name="Name" id="name" bind:value={name}>
+
+<label for="color">Color Values: {colorValue}</label>
