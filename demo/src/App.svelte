83957e1700f4198787c10b38c943e790341a2187
Tanmay Jain <tanmayj28@gmail.com>
2024-04-21 19:09:05 +0530

    using ID with each loop and how to get index on loop

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -6,14 +6,19 @@
   }, 2000);
 
   function addToList() {
-    colorList = [...colorList, color];
+    if(colorList.includes(color)) {
+      color = '';
+      return alert('Duplicate Key Entered!')
+    }
+    colorList = [color, ...colorList];
     color = '';
   }
 </script>
 
 <ul>
-  {#each colorList as color}
-  <li>{color}</li>
+  <!-- Since any update to colorList triggers a re-render of all list items, we can pass a unique key like here as color and only re-render based on the unique key. -->
+  {#each colorList as color, index (color)}
+  <li>{index+1}). {color}</li>
   {/each}
 </ul>
 
