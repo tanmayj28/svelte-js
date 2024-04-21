91f8d9473ed353831d923ff7f9bd6fcb9d7c803a
Tanmay Jain <tanmayj28@gmail.com>
2024-04-21 16:46:27 +0530

    reactive variable hexcolor added

--- color-picker/src/ColorDisplay.svelte
+++ color-picker/src/ColorDisplay.svelte
@@ -2,6 +2,17 @@
   export let reddish;
   export let greeinsh;
   export let blueish;
+
+  function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+  }
+
+  function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  }
+
+  $: rbgHex = rgbToHex(reddish, greeinsh, blueish);
 </script>
 
 <div class="row">
@@ -9,6 +20,8 @@
     <div id="color" style="background-color: rgb({reddish}, {greeinsh}, {blueish});"></div>
   </div>
 </div>
+
+<label for="hexValue">Hex: {rbgHex}</label>
 
 <style>
   #color {
