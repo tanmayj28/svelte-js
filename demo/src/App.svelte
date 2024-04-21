433474cdc439bed96ecd1297a31beed203b3fef5
Tanmay Jain <tanmayj28@gmail.com>
2024-04-21 17:09:37 +0530

    if else statement

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,34 +1,21 @@
-<!-- Reactive Statement: So, if anything on the right changes, it would reload the variable on left or rerun the code as seen in below console.log logic. -->
-
 <script>
-  let name = 'Tanmay';
-  let initName = name.toUpperCase();
-  $: capitalizedName = name.toUpperCase();
-
-  let red = 10;
-  let green = 0;
-  let blue = 0;
-  $: colorValue = color(red, green, blue);
-
-  function color(r,g,b) {
-    return `rgb(${r},${g},${b})`;
-  }
-
-  setTimeout(() => {
-    green += 10;
-  },1500);
-
-  $: console.log(green);
-  $: {
-    console.log(green, 'Green Value Updates');
-    console.log(red, 'Red Value Same');
-  }
+  let colorName = '';
+  let isChecked = false;
 </script>
 
-<h2>Name: {name}</h2>
-<h2>Name: {initName}</h2>
-<h2>Name: {capitalizedName}</h2>
+{#if colorName.toLowerCase() === 'red'}
+  <h2>Red</h2>
+  {:else if colorName.toLowerCase() === 'green'}
+  <h2>Green</h2>
+  {:else if colorName.toLowerCase() === 'blue' }
+  <h2>Blue</h2>
+  {:else}
+  <h2>Not RGB!</h2>
+{/if}
 
-<input type="text" name="Name" id="name" bind:value={name}>
-
-<label for="color">Color Values: {colorValue}</label>
+<input type="text" name="color" id="color" bind:value={colorName}>
+<input type="checkbox" bind:checked={isChecked}>
+<hr>
+{#if isChecked}
+<h2>You Checked it yo!!</h2>
+{/if}
