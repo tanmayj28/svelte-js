ab7afcba4aec2954b9b1df8ca4365c47f5b05fed
Tanmay Jain <tanmayj28@gmail.com>
2024-04-21 18:13:00 +0530

    each statement for looping over arrays

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,21 +1,23 @@
 <script>
-  let colorName = '';
-  let isChecked = false;
+  let colorList = ['red', 'green', 'blue'];
+  let color = '';
+  setTimeout(() => {
+    colorList = [...colorList, 'white'];
+  }, 2000);
+
+  function addToList() {
+    colorList = [...colorList, color];
+    color = '';
+  }
 </script>
 
-{#if colorName.toLowerCase() === 'red'}
-  <h2>Red</h2>
-  {:else if colorName.toLowerCase() === 'green'}
-  <h2>Green</h2>
-  {:else if colorName.toLowerCase() === 'blue' }
-  <h2>Blue</h2>
-  {:else}
-  <h2>Not RGB!</h2>
-{/if}
+<ul>
+  {#each colorList as color}
+  <li>{color}</li>
+  {/each}
+</ul>
 
-<input type="text" name="color" id="color" bind:value={colorName}>
-<input type="checkbox" bind:checked={isChecked}>
-<hr>
-{#if isChecked}
-<h2>You Checked it yo!!</h2>
+<input type="text" name="color" id="color" bind:value={color}>
+{#if color !== ''}
+  <button type="submit" on:click={addToList}>Add Color</button>
 {/if}
