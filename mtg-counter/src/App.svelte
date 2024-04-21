8a587760b12ee365f18efdb6734d4b925d11aa45
Tanmay Jain <tanmayj28@gmail.com>
2024-04-22 00:02:38 +0530

    initial commit :: MTG Counter :: adds UI

---
+++ mtg-counter/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import TeamBoard from "./TeamBoard.svelte";
+</script>
+
+<div class="container">
+  <div class="row">
+    <h1 style="padding-left:40%;">MTG Counter</h1>
+  </div>
+  <div class="row">
+    <TeamBoard name="Red" color="red"/>
+    <TeamBoard name="Blue" color="blue"/>
+  </div>
+  <div class="row" style="padding-top: 10px;">
+    <button style="width: 100%;background-color:orange;">Reset Game</button>
+  </div>
+</div>
---
+++ mtg-counter/src/TeamBoard.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let name;
+  export let color;
+  let score = 20;
+</script>
+
+<div class='col border-box' style="border: 2px dotted {color};">
+  <div class='container'>
+    <div class="row">
+      <h2 class='left-padding'>
+        Team {name}: {score}
+      </h2>
+    </div>
+
+    <div class="row btn-left-padding">
+      <button style="background-color: green;">
+        <h2>+</h2>
+      </button>
+      <button style="background-color: red;">
+        <h2>-</h2>
+      </button>
+    </div>
+  </div>
+</div>
+
+<style>
+  .border-box {
+    width: 100%;
+    height: 150px;
+  }
+
+  .left-padding {
+    padding-left: 35%;
+  }
+
+  .btn-left-padding {
+    padding-left: 45%;
+  }
+</style>
