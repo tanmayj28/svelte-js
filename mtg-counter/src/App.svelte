5d94b5f8bfc3a6480825062ff49f6e3c3a45ae8d
Tanmay Jain <tanmayj28@gmail.com>
2024-04-22 23:15:59 +0530

    adds counter logic functions and variables

--- mtg-counter/src/App.svelte
+++ mtg-counter/src/App.svelte
@@ -1,16 +1,52 @@
 <script>
   import TeamBoard from "./TeamBoard.svelte";
+  let redScore = 20;
+  let blueScore = 20;
+  $: redWon = blueScore === 0;
+  $: blueWon = redScore === 0;
+  $: gameOver = redWon || blueWon;
+
+  function resetGame() {
+    redScore = 20;
+    blueScore = 20;
+  }
 </script>
 
-<div class="container">
-  <div class="row">
-    <h1 style="padding-left:40%;">MTG Counter</h1>
-  </div>
-  <div class="row">
-    <TeamBoard name="Red" color="red"/>
-    <TeamBoard name="Blue" color="blue"/>
-  </div>
-  <div class="row" style="padding-top: 10px;">
-    <button style="width: 100%;background-color:orange;">Reset Game</button>
+<div class="row mt-2">
+  <div class="col">
+    <h1 class="text-center">MTG Counter</h1>
   </div>
 </div>
+<div class="row">
+  <TeamBoard name="Red" color="red" bind:score={redScore} {gameOver}/>
+  <TeamBoard name="Blue" color="blue" bind:score={blueScore} {gameOver}/>
+</div>
+
+{#if !gameOver}
+<div class="row mt-3">
+  <div class="col">
+    <button class="btn btn-warning w-100" on:click={resetGame}>Reset Game</button>
+  </div>
+</div>
+{:else}
+<div class="row">
+  <div class="col mt-2">
+    {#if blueWon}
+      <h2 class="text-center text-primary">
+        Blue Won!
+      </h2>
+    {:else}
+      <h2 class="text-center text-danger">
+        Red Won!
+      </h2>
+    {/if}
+  </div>
+</div>
+<div class="row mt-1">
+  <div class="col">
+    <button class="btn btn-success w-100" on:click={resetGame}>New Game</button>
+  </div>
+</div>
+
+{/if}
+i love you
--- mtg-counter/src/TeamBoard.svelte
+++ mtg-counter/src/TeamBoard.svelte
@@ -1,22 +1,32 @@
 <script>
   export let name;
   export let color;
-  let score = 20;
+  export let score;
+  export let gameOver;
+
+  function add() {
+   score += 1; 
+  }
+
+  function minus() {
+   score -= 1; 
+  }
 </script>
 
-<div class='col border-box' style="border: 2px dotted {color};">
+<div class='col border-box' style="border: dashed 5px {color};">
   <div class='container'>
     <div class="row">
-      <h2 class='left-padding'>
+      <h2 class='left-padding'
+      class:text-danger={name === 'Red'} class:text-primary={name === 'Blue'}>
         Team {name}: {score}
       </h2>
     </div>
 
     <div class="row btn-left-padding">
-      <button style="background-color: green;">
+      <button class="btn btn-success" on:click={add} disabled={gameOver}>
         <h2>+</h2>
       </button>
-      <button style="background-color: red;">
+      <button class="btn btn-danger" on:click={minus} disabled={gameOver}>
         <h2>-</h2>
       </button>
     </div>
