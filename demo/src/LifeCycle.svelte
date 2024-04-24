d5bf9c8f08f2ee418cb45d4c2706690d5897d8e9
Tanmay Jain <tanmayj28@gmail.com>
2024-04-24 23:46:51 +0530

    adds auto-scroll chat like :: lifecycle example usecase

--- demo/src/LifeCycle.svelte
+++ demo/src/LifeCycle.svelte
@@ -1,10 +1,17 @@
 <script>
   import { onMount, onDestroy, beforeUpdate, afterUpdate } from "svelte";
+  let numbers = [];
+  let autoScroll = false;
+  let unorderedListElement;
 
   onMount(() => {
-    console.log('Mounted!')
+    console.log('mounted!')
+    const intervalId = setInterval(() =>{
+      numbers = [...numbers, numbers.length + 1];
+    }, 1000);
 
     return () => {
+      clearInterval(intervalId);
       console.log('Also destroyed! (but last)!')
     }
   });
@@ -14,7 +21,10 @@
   });
 
   afterUpdate(() => {
-    console.log('after Update!!')
+    console.log('after Update!!');
+    if(autoScroll) {
+      unorderedListElement.scrollTo(0, unorderedListElement.scrollHeight);
+    }
   });
 
   onDestroy(() => {
@@ -23,3 +33,18 @@
 </script>
 
 <h2>Mounted!</h2>
+
+<ul bind:this={unorderedListElement}>
+  {#each numbers as number (number)}
+    <li>{number}</li>
+  {/each}
+</ul>
+<label>Autoscroll: <input type="checkbox" bind:checked={autoScroll}></label>
+
+<style>
+  ul {
+    max-height: 100px;
+    border: solid black 1px;
+    overflow: scroll;
+  }
+</style>
