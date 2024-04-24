60b95300556aa334ce72cf530ecc8396bb87286e
Tanmay Jain <tanmayj28@gmail.com>
2024-04-24 22:34:08 +0530

    adds onMount onDestroy basics :: 01 using checkbox

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,25 +1,10 @@
 <script>
-  async function getQoute() {
-    const url = 'https://zenquotes.io/api/random';
-    const response = await fetch(url);
-    const [quoteInfo] = await response.json();
-    return quoteInfo;
-  }
-
-  let promiseQuote = getQoute();
-
-  function refreshQoute() {
-    promiseQuote = getQoute();
-  }
+  import LifeCycle from "./LifeCycle.svelte";
+  let checked = false;
 </script>
 
-{#await promiseQuote}
-  <h2>Loading Quote. . . .</h2>
-{:then quoteInfo} 
-  <h2>{quoteInfo.q}</h2>
-  <h2>Author: {quoteInfo.a}</h2>
-{:catch error}
-  <h2>Error: {error.message}</h2>
-{/await}
+<input type="checkbox" bind:checked={checked}>
 
-<button on:click={refreshQoute}>Refresh Quote</button>
+{#if checked}
+  <LifeCycle />
+{/if}
---
+++ demo/src/LifeCycle.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount, onDestroy } from "svelte";
+
+  onMount(() => {
+    console.log('mounted!!');
+
+    return () => {
+      console.log('Also runs on destroy but after the onDestroy');
+    }
+  })
+
+  onDestroy(() => {
+    console.log('destroyed!!')
+  })
+</script>
+
+<h2>Checked!</h2>
