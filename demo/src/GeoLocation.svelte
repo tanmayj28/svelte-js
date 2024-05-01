90452c29f3f4c7a4cad2c35ef8e16d16d4ce5237
tajain-mdsol <tajain@mdsol.com>
2024-05-01 16:27:04 +0530

    no need to unsub a store if you are using the $store syntax

--- demo/src/GeoLocation.svelte
+++ demo/src/GeoLocation.svelte
@@ -4,13 +4,28 @@
 
   // store's subscribe function returns a unsubcribe function, so we can call it when we destroy the component.
 
-  const unsub = GeoLocationStore.subscribe(info => {
-    console.log('info from GL store', info);
-  });
+  // const unsub = GeoLocationStore.subscribe(info => {
+  //   console.log('info from GL store', info);
+  // });
 
-  onDestroy(() => {
-    // Unsubscribing prevents memory leak issues, as we  don't want to keep the component alive forever.
-    console.log('Unsubscribing. . .', unsub);
-    unsub();
-  })
+  // onDestroy(() => {
+  //   // Unsubscribing prevents memory leak issues, as we  don't want to keep the component alive forever.
+  //   console.log('Unsubscribing. . .', unsub);
+  //   unsub();
+  // })
 </script>
+
+<!-- with the $ sign syntax, Svelte will unsubscribe for you. -->
+{#if !$GeoLocationStore.location && !$GeoLocationStore.errMsg}
+  <h2>
+    Waiting for your location...
+  </h2>
+{:else if $GeoLocationStore.errMsg}
+  <h2>
+    Error: {$GeoLocationStore.errMsg}
+  </h2>
+{:else}
+  <h2>
+    Your current position is at latitude: {$GeoLocationStore.lat}, longitude:{$GeoLocationStore.long}.
+  </h2>
+{/if}
