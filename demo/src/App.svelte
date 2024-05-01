0edfd4c8766a4954775454de2c986c414ba9c55d
tajain-mdsol <tajain@mdsol.com>
2024-05-02 00:57:51 +0530

    adds writeable store example using counter

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,10 +1,36 @@
 <script>
-  import GeoLocation from "./GeoLocation.svelte";
-  let checked = false;
+  import countStore from "./stores/count.store";
+  import { onDestroy } from 'svelte';
+  const unsub = countStore.subscribe((number) => {
+    console.log(number, 'Count Store');
+  });
+
+  // This would just set a new value for the  number in the store.
+  // setTimeout(() => {
+  //   countStore.set(20);
+  // }, 2000);
+
+  // This takes and updates the existing store value, i.e. adds 1 to the existing count. Update can take in an anonyous function as input.
+  // setTimeout(() => {
+  //   countStore.update(n => n + 1);
+  // }, 2000);
+
+  onDestroy(() => {
+    unsub();
+  });
+
+  function addToStore() {
+    countStore.update(n => n + 1);
+  }
+
+  function subtractTFromStore() {
+    countStore.update(n => n - 1);
+  }
 </script>
 
-<input type="checkbox" bind:checked={checked}>
+<h1>
+  {$countStore}
+</h1>
 
-{#if checked}
-  <GeoLocation />
-{/if}
+<button on:click={addToStore}>Add</button>
+<button on:click={subtractTFromStore}>Subtract</button>
