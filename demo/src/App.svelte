7941422e9e2af113d502872fcbfca26eca65e6ee
tajain-mdsol <tajain@mdsol.com>
2024-05-25 20:36:08 +0530

    Slot :: creating basic and named Slots

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,38 +1,11 @@
-<!-- An action is a function that gets run when an HTML Element loads on a page. -->
 <script>
-  let show = false;
-  let width = 100;
-  function exampleAction(node, {color, width}) {
-    console.log(node);
-    const contextFunc = (e) => {
-      e.preventDefault();
-      alert('Right Click!');
-    };
-    node.addEventListener('contextmenu', contextFunc);
-    node.style.backgroundColor = color;
-    node.style.width = width + 'px';
-    return {
-      destroy() {
-        console.log('Destroy was called!');
-        node.removeEventListener('contextmenu', contextFunc);
-      },
-      update({color, width}){
-        node.style.backgroundColor = color;
-        node.style.width = width + 'px';
-      }
-    }
-  }
+  import Box from './Box.svelte';
 </script>
 
-<input type="checkbox" bind:checked={show} />
-{#if show}
-  <div use:exampleAction={{color: '#AA0000', width}} />
-{/if}
-<input type="range" min=50 max=500 bind:value={width}>
-<style>
-  div {
-    width: 300px;
-    height: 300px;
-    border: solid black 1px;
-  }
-</style>
+<Box>
+  <h1 slot="top">Hello world!</h1>
+  <div slot="bottom">
+    <p>How are you doing?</p>
+  </div>
+  <p slot="middle">Hi!! All gucci.</p>
+</Box>
---
+++ demo/src/Box.svelte
@@ -0,0 +1,19 @@
+<div>
+  <slot name="top" />
+</div>
+
+<div>
+  <slot name="middle" />
+</div>
+
+<div>
+  <slot name="bottom" />
+</div>
+
+<style>
+  div {
+    width: 500px;
+    min-height: 300px;
+    border: 1px solid gray;
+  }
+</style>
